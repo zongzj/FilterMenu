57397ad5b276016d3c39c7d1d23eeda9541096e1
zzj <zhijie.zong@yunhuyj.com>
2019-10-16 18:02:28 +0800

    抽取lib

---
+++ app/src/main/java/com/zong/example/MainActivity.java
@@ -0,0 +1,56 @@
+package com.zong.example;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.zong.filter.CategoryBean;
+import com.zong.filter.DialogFilterFragment;
+import com.zong.filter.DialogFilterUtils;
+import com.zong.filter.GetJsonDataUtil;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String JsonData = new GetJsonDataUtil().getJson(this, "menu.json");//获取assets目录下的json文件数据
+        final DialogFilterUtils dialogFilterUtils = new DialogFilterUtils();
+        final List<CategoryBean> categoryList = new Gson().fromJson(JsonData, new TypeToken<List<CategoryBean>>() {
+        }.getType());
+        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                 dialogFilterUtils.filterDialog(MainActivity.this, categoryList, new View.OnClickListener() {
+                     @Override
+                     public void onClick(View v) {
+                         for (CategoryBean bean : categoryList) {
+                             for (CategoryBean.ChildBean childBean : bean.getChild()) {
+                                 if(childBean.isIsCheck()){
+                                     Log.i(TAG, "选中id: " + childBean.getId());
+                                     Log.i(TAG, "选中名称: " + childBean.getTypename());
+                                 }
+                             }
+                         }
+
+                     }
+                 });
+//                DialogFilterFragment filterFragment = null;
+//                filterFragment = DialogFilterFragment.newInstance(categoryList);
+//                filterFragment.show(getSupportFragmentManager(), "fl");
+
+
+            }
+        });
+
+    }
+}
