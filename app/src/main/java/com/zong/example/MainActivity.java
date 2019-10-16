22b6036fe219a91103e8d97bf1ef29c620875515
zzj <zhijie.zong@yunhuyj.com>
2019-10-16 18:53:58 +0800

    抽取lib

--- app/src/main/java/com/zong/example/MainActivity.java
+++ app/src/main/java/com/zong/example/MainActivity.java
@@ -24,30 +24,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         String JsonData = new GetJsonDataUtil().getJson(this, "menu.json");//获取assets目录下的json文件数据
         final DialogFilterUtils dialogFilterUtils = new DialogFilterUtils();
+        //注意:数据初始化一次就好. 不然菜单选择数据会清空
         final List<CategoryBean> categoryList = new Gson().fromJson(JsonData, new TypeToken<List<CategoryBean>>() {
         }.getType());
         findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                dialogFilterUtils.filterDialog(MainActivity.this, categoryList, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        for (CategoryBean bean : categoryList) {
+                            for (CategoryBean.ChildBean childBean : bean.getChild()) {
+                                if (childBean.isIsCheck()) {
+                                    Log.i(TAG, "选中id: " + childBean.getId());
+                                    Log.i(TAG, "选中名称: " + childBean.getTypename());
+                                }
+                            }
+                        }
 
-                 dialogFilterUtils.filterDialog(MainActivity.this, categoryList, new View.OnClickListener() {
-                     @Override
-                     public void onClick(View v) {
-                         for (CategoryBean bean : categoryList) {
-                             for (CategoryBean.ChildBean childBean : bean.getChild()) {
-                                 if(childBean.isIsCheck()){
-                                     Log.i(TAG, "选中id: " + childBean.getId());
-                                     Log.i(TAG, "选中名称: " + childBean.getTypename());
-                                 }
-                             }
-                         }
-
-                     }
-                 });
-//                DialogFilterFragment filterFragment = null;
-//                filterFragment = DialogFilterFragment.newInstance(categoryList);
-//                filterFragment.show(getSupportFragmentManager(), "fl");
-
+                    }
+                });
 
             }
         });
